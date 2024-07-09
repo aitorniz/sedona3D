cfc63707042fe8359d9ccec963f8f1b541aa30a9
aitorniz <aitorlopez@orange.fr>
2024-07-09 14:33:11 +0200

    My home made example works

--- common/src/main/scala/Main.scala
+++ common/src/main/scala/Main.scala
@@ -3,20 +3,28 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.sedona.spark.SedonaContext
 import org.apache.sedona.viz.core.Serde.SedonaVizKryoRegistrator
 import org.apache.sedona.viz.sql.utils.SedonaVizRegistrator
-import org.apache.spark.textFile
+import org.apache.spark.sql.SparkSession
 
-import GeometryType3D._
-import Point3DFormatMapper._
+import Point3DRDD.Point3DRDD
+
 
 object Main extends App {
   Logger.getRootLogger().setLevel(Level.WARN)
 
-  val config = SedonaContext.builder().appName("Sedona3D ----- demo")
-    .master("local[*]") // Please comment out this when use it on a cluster
-    .config("spark.kryo.registrator", classOf[SedonaVizKryoRegistrator].getName)
-    .getOrCreate()
-  val sedona = SedonaContext.create(config)
+  //val config = SedonaContext.builder().appName("Sedona3D ----- demo")
+  //  .config("spark.kryo.registrator", classOf[SedonaVizKryoRegistrator].getName)
+  //  .getOrCreate()
+  //val sedona = SedonaContext.create(config)
+
+  val spark = SparkSession.builder()
+      .appName("Read csv example")
+      .getOrCreate()
+
+  val Point3Dcsv = spark.read.option("lineSep", "\n").option("delimiter", ",").csv("./data/testCSV3D.csv")
+
+  Point3Dcsv.show()
+  
 
   //call the main operation
-  Point3DRDD()
+  //Point3DRDD()
 }
