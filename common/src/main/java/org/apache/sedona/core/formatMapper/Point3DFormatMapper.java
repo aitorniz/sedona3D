ad1611f254286f1152f162017b19a49bb15773ad
aitorniz <aitorlopez@orange.fr>
2024-07-11 08:10:54 +0200

    POINT3D import bug unfixed

--- common/src/main/java/org/apache/sedona/core/formatMapper/Point3DFormatMapper.java
+++ common/src/main/java/org/apache/sedona/core/formatMapper/Point3DFormatMapper.java
@@ -1,11 +1,32 @@
+package FormatMapper;
 
-import org.apache.sedona.core.formatMapper.PointFormatMapper
-import 
+import org.apache.sedona.core.formatMapper.PointFormatMapper;
+import org.apache.sedona.common.enums.FileDataSplitter;
+import GeometryType3D.POINT3D;
+
 public class Point3DFormatMapper extends PointFormatMapper{
-	@override
-	public PointFormatMapper(Integer startOffset, FileDataSplitter Splitter, boolean carryInputData) {
-	    super(startOffset, startOffset + 1, Splitter, carryInputData, GeometryType.POINT3D);
+	 /**
+   	* Instantiates a new point format mapper.
+   	*
+        * @param Splitter the splitter
+	* @param carryInputData the carry input data
+        */
+	
+  	public Point3DFormatMapper(FileDataSplitter Splitter, boolean carryInputData) {
+    	    super(0, 1, Splitter, carryInputData, GeometryType3D.POINT3D);
+  	}
+	/**
+        * Instantiates a new point format mapper.
+        *
+        * @param startOffset the start offset
+        * @param Splitter the splitter
+        * @param carryInputData the carry input data
+        */
+	
+	public Point3DFormatMapper(Integer startOffset, FileDataSplitter Splitter, boolean carryInputData) {
+	    super(startOffset, startOffset + 1, Splitter, carryInputData, GeometryType3D.POINT3D);
 
 	}
+}
 
 
